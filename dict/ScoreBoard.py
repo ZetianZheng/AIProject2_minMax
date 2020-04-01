1dd97ccf5b07f663d8cd76787d3feb2847d2c6cc
Siyu <13501954683@163.com>
2020-04-01 15:15:28 -0400

    some improvements -- Siyu

--- dict/ScoreBoard.py
+++ dict/ScoreBoard.py
@@ -2,11 +2,17 @@ import pickle
 
 class ScoreBoard():
 
-    def  __init__(self, code_dict, score_dict):
+    def  __init__(self, m):
         # code_dict = code6.pkl, code8.pkl
         # score_dict = 6.pkl,8.pkl
-        self.code_dict = self.load_code_dict(code_dict)
-        self.score_board = self.load_score_dict(score_dict)
+        if m == 6:
+            self.O_code_dict = self.load_code_dict('O_code_6.pkl')
+            self.X_code_dict = self.load_code_dict('X_code_6.pkl')
+            self.score_board = self.load_score_dict('6.pkl')
+        elif m == 8:
+            self.O_code_dict = self.load_code_dict('O_code_8.pkl')
+            self.X_code_dict = self.load_code_dict('X_code_8.pkl')
+            self.score_board = self.load_score_dict('8.pkl')
 
     def load_code_dict(self, code_dict):
         with open (code_dict, 'rb') as f:
@@ -18,7 +24,7 @@ class ScoreBoard():
             score_board = pickle.load(f)
             return score_board
 
-    def decode(self, input_array):
+    def decode(self, input_array, symbol):
         '''
         decode strings in the input_array
 
@@ -28,14 +34,19 @@ class ScoreBoard():
         '''
         n = len(input_array)
         decode_array = []
+        if symbol == 'X':
+            code_dict = self.X_code_dict
+        elif symbol == 'O':
+            code_dict = self.O_code_dict
+
         for i in range(n):
             encoded_string = input_array[i]
-            decoded_string = self.code_dict[encoded_string]
+            decoded_string = code_dict[encoded_string]
             decode_array.append(decoded_string)
 
         return decode_array
 
-    def get_score(self, input_array):
+    def get_score(self, input_array, symbol):
         '''
         according to the current state, this function will calculate a score
 
@@ -44,7 +55,7 @@ class ScoreBoard():
 
         '''
         # change to new array
-        decode_array = self.decode(input_array)
+        decode_array = self.decode(input_array, symbol)
 
         # get the score
         score = 0
